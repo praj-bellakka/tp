1658262a5bf7f1d156e4674529d297b9235f605b
kengjit <kengjit123@gmail.com>
2021-10-03 22:11:06 +0800

    Improve Code

--- src/main/java/fitnus/Database.java
+++ src/main/java/fitnus/Database.java
@@ -3,5 +3,5 @@ package fitnus;
 import java.util.ArrayList;
 
 public class Database {
-    public static ArrayList<Food> databaseFoods;
+    public static ArrayList<Food> databaseFoods = new ArrayList<>();
 }
