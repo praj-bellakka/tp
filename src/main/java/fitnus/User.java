228a02effd42d25b5dd0fc47a0738f1241101567
siyuancheng178 <siyuanc6794@gmail.com>
2021-10-05 22:27:32 +0800

    add entry database

--- src/main/java/fitnus/User.java
+++ src/main/java/fitnus/User.java
@@ -3,7 +3,7 @@ package fitnus;
 import java.util.ArrayList;
 
 public class User {
-    public static ArrayList<TrackerEntry> trackerEntries = new ArrayList<>();
+    public static ArrayList<EntryDatabase> trackerEntries = new ArrayList<>();
     public static Integer calorieGoal;
     public static Integer caloriesRemaining;
     public static String gender;
