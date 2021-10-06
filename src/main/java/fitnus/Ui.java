34ea1f1f79739fbebbabf8388bcadc9b98b8ec1e
kengjit <69455772+kengjit@users.noreply.github.com>
2021-10-06 16:06:42 +0800

    Fix checkstyle

--- src/main/java/fitnus/Ui.java
+++ src/main/java/fitnus/Ui.java
@@ -4,22 +4,23 @@ public class Ui {
     public Ui() {
     }
 
-    public static final String WELCOME_MESSAGE = "------------------ \n" +
-                                                "Welcome to FitNUS Tracker!";
-    public static final String HELP_MESSAGE = "------------------ \n" +
-                                            "Here are some commands that you can use!\n" +
-                                            "------------------ \n" +
-                                            "[X] List out available commands: help\n" +
-                                            "[X] List foods in database: list food\n" +
-                                            "[X] Add food entry from database: add /def INDEX_OF_FOOD" +
-                                            "[X] Add custom food entry: add FOOD_NAME /cal CALORIES\n" +
-                                            "[X] Delete food entry: remove food INDEX_OF_FOOD\n" +
-                                            "[X] Set Gender: gender M/F (Select one)\n" +
-                                            "[X] View food intake for the day: list intake /DAY\n" +
-                                            "[X] Set calorie goal: calorie set GOAL\n" +
-                                            "[X] View remaining calories for the day: calorie remain\n" +
-                                            "[X] Exit FitNUS: exit\n" +
+    public static final String WELCOME_MESSAGE = "------------------ \n" 
+                                                + "Welcome to FitNUS Tracker!";
+    public static final String HELP_MESSAGE = "------------------ \n" 
+                                            + "Here are some commands that you can use!\n" 
+                                            + "------------------ \n" +
+                                            + "[X] List out available commands: help\n" 
+                                            + "[X] List foods in database: list food\n" 
+                                            + "[X] Add food entry from database: add /def INDEX_OF_FOOD" 
+                                            + "[X] Add custom food entry: add FOOD_NAME /cal CALORIES\n" 
+                                            + "[X] Delete food entry: remove food INDEX_OF_FOOD\n" 
+                                            + "[X] Set Gender: gender M/F (Select one)\n" 
+                                            + "[X] View food intake for the day: list intake /DAY\n" 
+                                            + "[X] Set calorie goal: calorie set GOAL\n" 
+                                            + "[X] View remaining calories for the day: calorie remain\n" 
+                                            + "[X] Exit FitNUS: exit\n" 
                                             "------------------ \n";
+    
     public void printWelcomeMessage() {
         println(WELCOME_MESSAGE);
     }
