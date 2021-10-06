654f4e2ccc89a161a1594f73b678145bfb076872
praj-bellakka <prajbellakka@gmail.com>
2021-10-06 17:03:45 +0800

    Add command handler in FitNus

--- src/main/java/fitnus/FitNus.java
+++ src/main/java/fitnus/FitNus.java
@@ -1,9 +1,17 @@
 package fitnus;
 
+import fitnus.parser.Parser;
+
+import java.util.Scanner;
+
 public class FitNus {
     public static void main(String[] args) {
         FoodDatabase database = new FoodDatabase();
         EntryDatabase ed = new EntryDatabase();
+        Parser parser = new Parser();
+        String userInput;
+        Scanner in = new Scanner(System.in);
+        boolean canExit = false;
         try {
             Storage.createDirectoryAndFiles();
             Storage.initialiseDatabase(database);
@@ -11,6 +19,46 @@ public class FitNus {
             Storage.saveDatabase(database);
         } catch (Exception e) {
             System.out.println(e.getMessage());
+        }
+
+
+        /* Command handler
+        TODO: refactor handler to separate classes.
+         */
+        while (!canExit) {
+            try {
+                userInput = in.nextLine().trim();
+                String inputType = parser.parseInputType(userInput);
+                switch (inputType) {
+                    //TODO: Refactor strings to enum
+                case "exit":
+                    canExit = true;
+                    break;
+                case "add":
+                    break;
+                case "remove":
+                    //TODO: add remove function
+                    break;
+                case "list":
+                    //TODO: add list function
+                    break;
+                case "calorie":
+                    //TODO: add calorie function
+                    break;
+                case "gender":
+                    //TODO: add gender function
+                    break;
+                case "help":
+                    //TODO: add help function
+                    break;
+                default:
+                    System.out.println("couldnt find function");
+                    break;
+                }
+            } catch (NullPointerException e) {
+                System.out.println("Wrong format");
+                e.printStackTrace();
+            }
         }
     }
 }
