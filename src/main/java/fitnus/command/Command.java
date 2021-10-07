8a4a74c610d09b940abe2e3acf4beb279224f1a9
kengjit <kengjit123@gmail.com>
2021-10-07 13:31:36 +0800

    Update command execute format

--- src/main/java/fitnus/command/Command.java
+++ src/main/java/fitnus/command/Command.java
@@ -1,6 +1,10 @@
 package fitnus.command;
 
+import fitnus.EntryDatabase;
+import fitnus.FoodDatabase;
+import fitnus.User;
+
 public abstract class Command {
 
-    public abstract String execute();
+    public abstract String execute(EntryDatabase ed, FoodDatabase fd, User us);
 }
