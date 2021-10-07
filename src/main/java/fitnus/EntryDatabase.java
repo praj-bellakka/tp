62732100a4af85dc9de4f2c2b8de4d9b933c5f43
kengjit <kengjit123@gmail.com>
2021-10-07 13:52:53 +0800

    Add get entry from EntryDatabase

--- src/main/java/fitnus/EntryDatabase.java
+++ src/main/java/fitnus/EntryDatabase.java
@@ -54,21 +54,8 @@ public class EntryDatabase {
         }
     }
 
-    public void addFromString(FoodDatabase fd, String input) {
-        Parser p = new Parser();
-        if (input.contains("/def")) {
-            int index = p.parseIntegers(input);
-            addDefaultEntry(fd, index);
-        } else if (input.contains("/cal")) {
-            String foodName = p.parseFoodName(input);
-            int calories = p.parseIntegers(input);
-            Food customFood = new Food(foodName, calories);
-            addEntry(customFood);
-            fd.addFood(customFood);
-        } else {
-            //TODO include exception throw here instead of print
-            System.out.println("Wrong add format used!");
-        }
+    public Entry getEntryAtIndex(int index) {
+        return entries.get(index);
     }
 }
 
