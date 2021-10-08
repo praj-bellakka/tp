c6077e85e6a5a67a1df49e7a79560ada16153dab
aaapppsss <adelinebooqw@gmail.com>
2021-10-08 22:22:08 +0800

    fix list intake parsing

--- src/main/java/fitnus/command/ListFoodIntakeCommand.java
+++ src/main/java/fitnus/command/ListFoodIntakeCommand.java
@@ -8,15 +8,15 @@ import fitnus.User;
 public class ListFoodIntakeCommand extends Command {
     private String timeSpan;
 
+    public static final String DESCRIPTOR_DAY = "/day";
+
     public ListFoodIntakeCommand(String timeSpan) {
         this.timeSpan = timeSpan;
     }
 
     @Override
     public String execute(EntryDatabase ed, FoodDatabase fd, User us) {
-        Ui ui = new Ui();
-
-        if (timeSpan.equals("/day")) {
+        if (timeSpan.equals(DESCRIPTOR_DAY)) {
             String entries = ed.convertDatabaseToString();
             return entries;
         }
