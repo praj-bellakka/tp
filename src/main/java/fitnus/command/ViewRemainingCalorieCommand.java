699c63f42aad904a594887d02a8f977c4104ccb9
aaapppsss <adelinebooqw@gmail.com>
2021-10-08 22:52:40 +0800

    changes

--- src/main/java/fitnus/command/ViewRemainingCalorieCommand.java
+++ src/main/java/fitnus/command/ViewRemainingCalorieCommand.java
@@ -5,18 +5,13 @@ import fitnus.FoodDatabase;
 import fitnus.User;
 
 public class ViewRemainingCalorieCommand extends Command {
-    private User user;
-    private EntryDatabase edb;
-
-    public ViewRemainingCalorieCommand(User user, EntryDatabase edb) {
-        this.user = user;
-        this.edb = edb;
+    public ViewRemainingCalorieCommand() {
     }
 
     @Override
     public String execute(EntryDatabase ed, FoodDatabase fd, User us) {
         return String.format("The remaining calories before reaching the daily goal is %d",
-                user.showCaloriesRemaining(edb));
+                us.showCaloriesRemaining(ed));
     }
 
 }
