1c71bf397b4cd6e41bb635d73d97d62b2b3af773
praj-bellakka <prajbellakka@gmail.com>
2021-10-12 21:25:58 +0800

    Fix import in ParserTest

--- src/test/java/fitnus/ParserTest.java
+++ src/test/java/fitnus/ParserTest.java
@@ -15,7 +15,9 @@ import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class ParserTest {
