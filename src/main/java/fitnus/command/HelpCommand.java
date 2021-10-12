3224b0939e8266eeb3274b123ade9e45947169e0
praj-bellakka <prajbellakka@gmail.com>
2021-10-12 16:23:43 +0800

    Change capitalization of list intake in HelpCommand

--- src/main/java/fitnus/command/HelpCommand.java
+++ src/main/java/fitnus/command/HelpCommand.java
@@ -20,7 +20,7 @@ public class HelpCommand extends Command {
                 + "[X] Add custom food entry: add /cust FOOD_NAME|CALORIES\n"
                 + "[X] Delete food entry: remove /food INDEX_OF_FOOD\n"
                 + "[X] Set Gender: gender /set M/F (Select one)\n"
-                + "[X] View food intake for the day: list /intake /DAY\n"
+                + "[X] View food intake for the day: list /intake /day\n"
                 + "[X] Set calorie goal: calorie /set GOAL\n"
                 + "[X] View remaining calories for the day: calorie /remain\n"
                 + "[X] Exit FitNUS: exit\n"
