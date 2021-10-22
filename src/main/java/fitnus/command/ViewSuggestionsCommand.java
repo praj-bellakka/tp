c5731fcb374e0239848d3ddcfe1652882ab18d70
brendanlsz <brendanlau2926@gmail.com>
2021-10-23 00:51:32 +0800

    Add optional sorting to suggestions

--- src/main/java/fitnus/command/ViewSuggestionsCommand.java
+++ src/main/java/fitnus/command/ViewSuggestionsCommand.java
@@ -11,14 +11,16 @@ import java.util.ArrayList;
 
 public class ViewSuggestionsCommand extends Command {
     private final Food.FoodType type;
+    private final boolean isSort;
 
-    public ViewSuggestionsCommand(Food.FoodType type) {
+    public ViewSuggestionsCommand(Food.FoodType type, boolean isSort) {
         this.type = type;
+        this.isSort = isSort;
     }
 
     public String execute(EntryDatabase ed, FoodDatabase fd, User us) throws FitNusException {
         int remaining = us.getCalorieGoal() - ed.getTotalDailyCalorie();
-        ArrayList<Food> suggestions = fd.findSuggestions(type, remaining);
+        ArrayList<Food> suggestions = fd.findSuggestions(type, remaining, isSort);
         Ui.printMatchingSuggestions(suggestions);
         return "";
     }
